fa40a22bdd684bbd523189614f3927a402113e6c
Renan <renan0499@hotmail.com>
2021-06-14 17:45:15 +0100

    pagina promotor

---
+++ app/Projeto/Projeto/PromotorP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto
+{
+    public partial class Promotor
+    {
+        public override string ToString()
+        {
+            return string.Format("Nome: {0} NIF: {1} ", Nome, NIF);
+        }
+    }
+}
