9808dda19e69848bb49c42fd65335f9167915955
iuricarras <iuri.carrasqueiro@gmail.com>
2021-06-23 19:47:18 +0100

    ProcessoP

---
+++ app/Projeto/Projeto/ProcessoP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto
+{
+    public partial class Processo
+    {
+        public override string ToString()
+        {
+
+            return string.Format("Processo Nº {0} - Data {1}", this.Id, this.DataInicio);
+        }
+    }
+}
